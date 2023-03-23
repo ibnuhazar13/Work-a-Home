c50c8de3c6fcbfff63275741189b17faa12f77ac
ibnuhazar13 <128708237+ibnuhazar13@users.noreply.github.com>
2023-03-23 20:08:26 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+	include"inc/config.php";
+	include"layout/header.php";
+	
+?>
+
+		<div class="container text-center" style="margin-top:20px;padding:50px;">		
+		<br>
+		<h1>"SELAMAT DATANG DI RESTO21"</h1>
+        <h3>Resto21 menyediakan berbagai makanan dan minuman siap antar Buka Pukul 10.00 s.d 22.00 WIB</h3>
+        <h2> Menerima Pesanan Catring</h3>
+        <h3> Untuk pemesanan Catring di pesan sehari sebelumnya atau 2 Jam sebelumnya</h3>
+        <h3> Untuk pembayaran pemesanan Catring di harap membayar DP 50% di Awal Pemesanan</h3>
+        <h3> Ongkir Pesanan Mulai dari Rp5000 - Rp10000</h3>
+    </div> <!-- /container -->
+		
+    <?php include"layout/footer.php"; ?>
